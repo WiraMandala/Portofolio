8228f9c08c92dfaec1a5c58d621a278d3430f999
Wira Mandala <wiramandala21@gmail.com>
2026-09-26 22:13:06 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-const ProjectDetail = lazy(() => import("./component/Project/ProjectDetail"));
+const ProjectDetail = lazy(() => import("./Component/Project/ProjectDetail"));
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Navbar from "./Component/Navbar";
