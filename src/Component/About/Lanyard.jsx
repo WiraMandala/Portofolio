eba0b6daace14e0e6ccd372e72195595d4b67690
Wira Mandala <wiramandala21@gmail.com>
2026-09-26 23:43:37 +0700

    initial commit

--- src/Component/About/Lanyard.jsx
+++ src/Component/About/Lanyard.jsx
@@ -64,7 +64,7 @@ export default function Lanyard({
     <div className="lanyard-wrapper">
       <Canvas
         camera={{ position: position, fov: fov }}
-        dpr={[1, isMobile ? 1.5 : 2]}
+        dpr={[1, isMobile ? 1 : 2]}
         gl={{ alpha: transparent }}
         onCreated={({ gl }) =>
           gl.setClearColor(new THREE.Color(0x000000), transparent ? 0 : 1)
